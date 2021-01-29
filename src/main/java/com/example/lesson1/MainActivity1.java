9b55f10ea12611d357eabfe7897d9b88e661339d
SupaMike2033 <nata_1477@mail.ru>
2021-01-29 15:23:33 +0300

    Initial commit

---
+++ src/main/java/com/example/lesson1/MainActivity1.java
@@ -0,0 +1,85 @@
+package com.example.lesson1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.EditText;
+import android.widget.Switch;
+import android.widget.TextView;
+import android.widget.ToggleButton;
+
+public class MainActivity1 extends AppCompatActivity {
+
+    TextView textView;
+    EditText editText;
+    Button button;
+    Switch switchButton;
+    CheckBox checkBox;
+    ToggleButton toggleButton;
+    String badText = "";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main1);
+
+        button = findViewById(R.id.button);
+        textView = findViewById(R.id.textView);
+        editText = findViewById(R.id.editText);
+        switchButton = findViewById(R.id.switchButton);
+        checkBox = findViewById(R.id.checkBox);
+        toggleButton = findViewById(R.id.toggleButton);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String tmpStr = editText.getText().toString();
+                if(tmpStr.equals("")) {
+                    tmpStr = "Обленился совсем???";
+                }
+                textView.setText(tmpStr + badText);
+                editText.setText("");
+            }
+        });
+
+        switchButton.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if(isChecked) {
+                    button.setVisibility(View.VISIBLE);
+                } else {
+                    button.setVisibility(View.INVISIBLE);
+                }
+            }
+        });
+
+        checkBox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if(isChecked) {
+                    badText = " ГАДОСТЬ";
+                } else {
+                    badText = "";
+                }
+            }
+        });
+
+        toggleButton.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if(isChecked) {
+                    switchButton.setVisibility(View.VISIBLE);
+                    checkBox.setVisibility(View.VISIBLE);
+                } else {
+                    switchButton.setVisibility(View.INVISIBLE);
+                    checkBox.setVisibility(View.INVISIBLE);
+                }
+            }
+        });
+    }
+
+}
---
+++ src/main/java/com/example/lesson1/MainActivity2.java
@@ -0,0 +1,46 @@
+package com.example.lesson1;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CalendarView;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.EditText;
+import android.widget.Switch;
+import android.widget.TextView;
+import android.widget.ToggleButton;
+
+public class MainActivity2 extends AppCompatActivity {
+
+    EditText editDate;
+    CalendarView calendarView;
+    String str;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+
+        editDate = findViewById(R.id.editDate);
+        calendarView = findViewById(R.id.calendarView);
+
+        calendarView.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
+            @Override
+            public void onSelectedDayChange(@NonNull CalendarView view, int year, int month, int dayOfMonth) {
+                editDate.setText("");
+                str = "";
+                if(dayOfMonth < 10) str ="0";
+                str += dayOfMonth + ".";
+                month++;
+                if(month < 10) str += "0";
+                str += month + "." + year;
+                editDate.setText(str);
+            }
+        });
+    }
+
+}
